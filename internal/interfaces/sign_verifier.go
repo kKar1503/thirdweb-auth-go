24a51b8afa2a5e20fbfaccfb151e75c3d4ce7059
Yam Kar Lok <94188697+kKar1503@users.noreply.github.com>
2024-03-14 10:51:04 +0800

    feat: Added a temporary wallet implementation
    This is temporary as a SignVerifier, until a wallet implementation is
    finished. This is just a replacement for the Wallet interface, which I
    don't immediately need yet

---
+++ internal/interfaces/sign_verifier.go
@@ -0,0 +1,14 @@
+package interfaces
+
+type Signer interface {
+	SignMessage(message string) (string, error)
+}
+
+type Verifier interface {
+	VerifySignature(message, signature string) (bool, error)
+}
+
+type SignVerifier interface {
+	Signer
+	Verifier
+}
